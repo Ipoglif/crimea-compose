326fd0e4bca7dca685655ca0897182382fedd54b
Ipoglif <Ipoglif@gmail.com>
2023-11-21 19:55:47 +0300

    1

--- up.ps1
+++ up.ps1
@@ -2,7 +2,8 @@ Write-Host "CrimeaServices UP"
 
 $upCommand = "docker compose " +
 "-f services/_base.yaml " +
-"-f services/bot.yaml " +
+"-f services/geteway.yaml " +
+"-f services/cities.yaml " +
 "--project-name crimea-services " +
 "up -d"
 
