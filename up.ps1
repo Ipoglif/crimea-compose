8e5b4d143ac84dd927fe994047d4ad7622e2765e
Ipoglif <Ipoglif@gmail.com>
2023-11-28 05:06:14 +0300

    add services auth and users

--- up.ps1
+++ up.ps1
@@ -4,6 +4,8 @@ $upCommand = "docker compose " +
 "-f services/_base.yaml " +
 "-f services/bot.yaml " +
 "-f services/geteway.yaml " +
+"-f services/auth.yaml " +
+"-f services/users.yaml " +
 "-f services/cities.yaml " +
 "--project-name crimea-services " +
 "up -d"
